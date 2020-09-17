e82d0874136cb3b46d13cbcd46a0e04057617f7d
Yuriy Semenkov <etoosamoe@yandex.ru>
2020-09-17 21:35:17 +0300

    add test

---
+++ gitlab-ci/infra/variables.tf
@@ -0,0 +1,48 @@
+variable cloud_id {
+  description = "Cloud"
+}
+variable folder_id {
+  description = "Folder"
+}
+variable zone {
+  description = "Zone"
+  # Значение по умолчанию
+  default = "ru-central1-a"
+}
+variable lb_count {
+  description = "Balancer Count"
+  default     = "1"
+}
+variable public_key_path {
+  # Описание переменной
+  description = "Path to the public key used for ssh access"
+}
+variable image_id {
+  description = "Disk image"
+}
+variable subnet_id {
+  description = "Subnet"
+}
+variable service_account_key_file {
+  description = "key .json"
+}
+variable private_key_path {
+  description = "path to private key"
+}
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default     = "reddit-app-base"
+}
+variable db_disk_image {
+  description = "Disk image for reddit db"
+  default     = "reddit-db-base"
+}
+variable storage_access_key {
+  description = "access_key id"
+}
+variable storage_secret_key {
+  description = "secret key"
+}
+variable storage_sa_id {
+  description = "SERVICE ACCOUNT ID"
+}
